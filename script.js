2640dfb3b6425f71a6c4b396326ea8f7954bff48
João Fortes <joaofortes502@gmail.com>
2022-05-10 22:31:34 -0300

    jone

---
+++ script.js
@@ -0,0 +1,36 @@
+const dino = document.getElementById("dino");
+const cactus = document.getElementById("cactus");
+
+function jump() {
+    if (dino.classList != "jump"){
+    dino.classList.add("jump");
+
+    setTimeout(function(){
+        dino.classList.remove("jump");
+    }, 300)
+ }
+}
+
+let vivo = setInterval(function() {
+    // posição Y do Dino
+    let dinoTop = parseInt(window.getComputedStyle(dino).getPropertyValue("top"));
+    
+    // posição X do Cacto
+    let cactusLeft = parseInt(window.getComputedStyle(cactus).getPropertyValue("left"));
+    
+    // colisão
+    if(cactusLeft <50 && cactusLeft > 0 && dinoTop >= 140)
+     // batida
+      alert("Você Perdeu!")
+
+
+
+}, 10);
+
+
+
+document.addEventListener("keydown", function(event) {
+    jump();
+    
+})
+
